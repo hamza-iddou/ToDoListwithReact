dc770b51e826a311b6cab34f74d3129f1636f4c6
Hamza Iddou <hamzaiddou2004@gmail.com>
2025-12-23 23:07:58 +0100

    fix some thing in the style also make the api fetch and setupt the local api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
+import axios from "axios";
 import SelectUser from "./Components/SelectUser";
 import Task from "./Components/Task";
+import { useEffect, useState } from "react";
+
+const api = axios.create({ baseURL: "http://localhost:3001" });
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [msg, setMsg] = useState("");
+  useEffect(() => {
+    setLoading(true);
+    api
+      .get("/todos")
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        setMsg("Error");
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <>
       <div className="container align-center ">
         <h1>Liste Des Todos</h1>
         <p>15 todo(s)</p>
         <hr />
-        <SelectUser/>
-        <Task />
-        
+        <SelectUser data={data}/>
+
+        {data.map((d , index) => (
+          <Task key={d.id}
+            userId={d.userId}
+            id={d.id}
+            title={d.title}
+            completed={d.completed}
+            number = {index}
+          />
+        ))}
       </div>
     </>
   );
--- src/Components/SelectUser.js
+++ src/Components/SelectUser.js
@@ -1,7 +1,9 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
-const SelectUser = () => {
+const SelectUser = ({data}) => {
+
+
   return (
     <>
       <p>
@@ -10,6 +12,7 @@ const SelectUser = () => {
       </p>
       <select class="form-select" aria-label="Default select example">
         <option selected value="-1">Tous les utlisateurs</option>
+       
         <option value="1">One</option>
         <option value="2">Two</option>
         <option value="3">Three</option>
